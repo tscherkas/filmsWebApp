46c86bae3243ef887c28c8ebbe636a7b82aeaf73
tscherkas <tscherkas.it@gmail.com>
2015-12-05 04:04:45 +0000

    Added DB project

---
+++ FilmsWebApp/FilmsWebUI/MoviesDB/dbo/Tables/KinopoistRelatedMovies.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[KinopoistRelatedMovies] (
+    [KinopoiskId]        BIGINT NOT NULL,
+    [RelatedKinopoiskId] BIGINT NOT NULL,
+    CONSTRAINT [PK_KinopoistRelatedMovies] PRIMARY KEY CLUSTERED ([KinopoiskId] ASC, [RelatedKinopoiskId] ASC)
+);
+
